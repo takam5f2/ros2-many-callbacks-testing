a20a2df25f5333caf4c2fba1e1b804587dace5a8
Takayuki AKAMINE <takayuki.akamine@tier4.jp>
2025-07-28 18:41:40 +0900

    add executor testing.
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>

--- executors_loader/launch/many_executors_loader.launch.py
+++ executors_loader/launch/many_executors_loader.launch.py
@@ -1,4 +1,4 @@
-import yaml # PyYAMLライブラリが必要です (pip install pyyaml)
+import yaml
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
@@ -10,7 +10,7 @@ from launch_ros.actions import Node
 def launch_setup(context, *args, **kwargs):
     
     # 1. LaunchConfigurationからYAMLファイルのパスを取得
-    config_file_path = LaunchConfiguration('node_config_file').perform(context)
+    config_file_path = LaunchConfiguration('executor_config_file').perform(context)
 
 
     # 2. YAMLファイルを読み込む
@@ -28,46 +28,40 @@ def launch_setup(context, *args, **kwargs):
     b_node:...
     """
     # 3. 読み込んだ設定から Node オブジェクトのリストを生成
-    nodes_to_launch = []
-    for node_name, node_info in config.items():
+    executors_to_launch = []
+    for executor_name in config.keys():
 
-        node_type = None
-        if node_info['type'] == 'listener':
-            node_type = 'simple_listener'
-        elif node_info['type'] == 'talker':
-            node_type = 'simple_talker'
-
-        nodes_to_launch.append(
+        executors_to_launch.append(
             Node(
-                package='simple_node',
-                executable=node_type,
-                name=node_name,
-                arguments=[node_name, config_file_path],
+                package='executors_loader',
+                executable='executors_loader',
+                namespace=executor_name,
+                arguments=[executor_name, config_file_path],
                 output='screen',
             )
         )
 
 
     # Nodeオブジェクトのリストを返す
-    return nodes_to_launch
+    return executors_to_launch
 
 
 def generate_launch_description():
     
     # 処理するYAMLファイルパスを受け取るための引数を宣言
-    package_share_dir = get_package_share_directory('simple_node')
-    default_config_path = package_share_dir + '/config/node_settings.yaml'
+    package_share_dir = get_package_share_directory('executors_loader')
+    default_config_path = package_share_dir + '/config/executor_setting.yaml'
     config_file_arg = DeclareLaunchArgument(
-        'node_config_file',
+        'executor_config_file',
         default_value=default_config_path,
         description='Nodes to launch, described in a YAML file.'
     )
 
     # OpaqueFunctionを使って、Launch実行時に上記のlaunch_setup関数を呼び出す
     # これにより、ユーザーが指定したファイルパスに基づいて動的に処理できる
-    dynamic_nodes = OpaqueFunction(function=launch_setup)
+    executor_loader_launch = OpaqueFunction(function=launch_setup)
 
     return LaunchDescription([
         config_file_arg,
-        dynamic_nodes
+        executor_loader_launch
     ])
---
+++ executors_loader/src/executors_loader.cpp
@@ -0,0 +1,134 @@
+#include "rclcpp/rclcpp.hpp"
+#include "yaml-cpp/yaml.h"
+
+#include "simple_node/simple_listener.hpp"
+#include "simple_node/simple_talker.hpp"
+#include <vector>
+
+rclcpp::Executor::SharedPtr create_executor(const YAML::Node & config)
+{
+  std::string executor_type = config["executor_type"].as<std::string>();
+  if (executor_type == "single_threaded_executor") {
+    return std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+  } else if (executor_type == "multi_threaded_executor") {
+    return std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+  } else {
+    RCLCPP_ERROR(rclcpp::get_logger("executors_loader"), "Unknown executor type: %s", executor_type.c_str());
+    throw std::runtime_error("Unknown executor type");
+  }
+}
+
+
+rclcpp::Node::SharedPtr create_simple_node(const YAML::Node & node_config)
+{
+  std::string node_name = node_config["node_name"].as<std::string>();
+  std::string node_type = node_config["node_type"].as<std::string>();
+  bool intra_process = node_config["intra_process"].as<bool>();
+
+  rclcpp::NodeOptions options;
+  options.use_intra_process_comms(intra_process);
+
+  if (node_type == "simple_talker") {
+    return std::make_shared<simple_node::SimpleTalker>(node_name, node_config, options);
+  } else if (node_type == "simple_listener") {
+    return std::make_shared<simple_node::SimpleListener>(node_name, node_config, options);
+  } else {
+    RCLCPP_ERROR(rclcpp::get_logger("executors_loader"), "Unknown node type: %s", node_type.c_str());
+    throw std::runtime_error("Unknown node type");
+  }
+}
+
+
+/*
+<YAML Format>
+executor_name:
+  executor_type: "single_threaded_executor" | "multi_threaded_executor"
+  nodes:
+    - node_name: "node_name_1"
+      node_type: simple_talker | simple_listener
+      node_namespace: <executor_name>_ns
+      intra_process: true | false
+      callbacks:
+        - topic: /topic/message/name0
+          frequency: 10
+          message: "Hello from name0"
+    - node_name: "node_name_1"
+      node_type: simple_listener | simple_talker
+      node_namespace: <executor_name>_ns
+      intra_process: true | false
+      callbacks:
+        - topic: /topic/message/name0
+
+
+<Example configuration for executors_loader>
+executor_00:
+  executor_type: "single_threaded_executor"
+  nodes:
+  - node_name: "simple_talker"
+    node_type: simple_talker
+    node_namespace: "executor_00_ns"
+    intra_process: false
+    callbacks:
+      - topic: /topic/message/name0
+        frequency: 10
+        message: "Hello from name0"
+  - node_name: "sample_listener"
+    node_type: simple_listener
+    node_namespace: "executor_00_ns"
+    intra_process: false
+    callbacks:
+      - topic: /topic/message/name0
+*/
+
+int main (int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+
+  // assign executor name from arguments[1]
+  // Load configuration from YAML file given by arguments[2]
+  if (argc < 3) {
+    RCLCPP_ERROR(rclcpp::get_logger("executors_loader"), "Usage: %s <executor name> <config_file>", argv[0]);
+    return 1;
+  }
+  std::string executor_name = argv[1];
+  std::string  config_file = argv[2];
+
+  YAML::Node config;
+  try {
+    config = YAML::LoadFile(config_file);
+  } catch (const YAML::Exception & e) {
+    RCLCPP_ERROR(rclcpp::get_logger("executors_loader"), "Failed to load configuration file: %s", e.what());
+    return 1;
+  }
+
+  if (!config[executor_name]) {
+    RCLCPP_ERROR(rclcpp::get_logger("executors_loader"), "Executor '%s' not found in configuration file", executor_name.c_str());
+    return 1;
+  }
+
+  auto executor = create_executor(config[executor_name]);
+
+  if (!executor) {
+    RCLCPP_ERROR(rclcpp::get_logger("executors_loader"), "Failed to create executor for '%s'", executor_name.c_str());
+    return 1;
+  }
+
+
+  // Create nodes based on the configuration.
+  std::vector<rclcpp::Node::SharedPtr> nodes;
+  for (const auto & node_config : config[executor_name]["nodes"]) {
+
+    rclcpp::Node::SharedPtr node = create_simple_node(node_config);
+    if (!node) {
+      RCLCPP_ERROR(rclcpp::get_logger("executors_loader"), "Failed to create node: %s", node_config["node_name"].as<std::string>().c_str());
+      return 1;
+    }
+    executor->add_node(node);
+    nodes.push_back(node);
+  }
+
+  executor->spin();
+
+  rclcpp::shutdown();
+  return 0;
+}
