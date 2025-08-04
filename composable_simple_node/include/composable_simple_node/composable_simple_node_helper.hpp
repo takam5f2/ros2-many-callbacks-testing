d2525edf78362ea957c32b6a0fb42c2f017c4c41
Takayuki AKAMINE <takayuki.akamine@tier4.jp>
2025-08-04 19:49:12 +0900

    chore(composable_simple_node): completed almost.
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>

---
+++ composable_simple_node/include/composable_simple_node/composable_simple_node_helper.hpp
@@ -0,0 +1,29 @@
+#ifndef __COMPOSABLE_SIMPLE_NODE_HELPER_HPP__
+#define __COMPOSABLE_SIMPLE_NODE_HELPER_HPP__
+
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+#include <yaml-cpp/yaml.h>
+
+namespace composable_simple_node
+{
+
+  YAML::Node load_yaml_config(rclcpp::Node* node)
+  {
+    std::string node_name{node->get_name()};
+    node->declare_parameter<std::string>("node_config_file", "config.yaml");
+    std::string node_config_file;
+    node->get_parameter("node_config_file", node_config_file);
+    RCLCPP_INFO(node->get_logger(), "Loading YAML config from: %s", node_config_file.c_str());
+
+    try {
+      YAML::Node config = YAML::LoadFile(node_config_file);
+      return config[node_name];
+    } catch (const YAML::Exception &e) {
+      RCLCPP_ERROR(node->get_logger(), "Failed to load YAML file: %s", e.what());
+      throw;
+    }
+  }
+}
+
+#endif // __COMPOSABLE_SIMPLE_NODE_HELPER_HPP__
