aed500d07ed90434b9ebc5e4c475018b0e18ce9b
Takayuki AKAMINE <takayuki.akamine@tier4.jp>
2025-08-13 18:47:28 +0900

    feat(simple_node): add configuration to change message length.
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>

--- simple_node/include/simple_node/simple_talker.hpp
+++ simple_node/include/simple_node/simple_talker.hpp
@@ -9,20 +9,34 @@
 
 namespace simple_node
 {
-class SimpleTalker : public rclcpp::Node
-{
-public:
-  SimpleTalker(const std::string &default_node_name, const YAML::Node &config = YAML::Node(),
-               const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
-  virtual ~SimpleTalker() override;
-  int init(const YAML::Node &config);
-private:
-  // Multiple publishers for different IDs by std::vector
-  std::vector<rclcpp::Publisher<std_msgs::msg::String>::SharedPtr> publishers_{};
-  // Multiple timers for different IDs by std::vector
-  std::vector<rclcpp::TimerBase::SharedPtr> timers_{};
-  std::vector<unsigned int> publishing_counters_{}; // Store frequencies for each timer
-};
+  enum MessagePattern : unsigned int
+  {
+    GIVEN_MESSAGE = 0,
+    REPEAT = 1,
+    RANDOM = 2,
+  };
+
+  class SimpleTalker : public rclcpp::Node
+  {
+  public:
+    SimpleTalker(const std::string &default_node_name, const YAML::Node &config = YAML::Node(),
+                 const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
+    virtual ~SimpleTalker() override;
+
+  private:
+    int init(const YAML::Node &config);
+    void reserve_message(const YAML::Node &config);
+    void repeat_message(const std::string &given_message, const size_t &message_byte_size, std::string &message_text);
+    void random_message(const std::string &given_message, const size_t &message_byte_size, std::string &message_text);
+
+    // Multiple publishers for different IDs by std::vector
+    std::vector<rclcpp::Publisher<std_msgs::msg::String>::SharedPtr> publishers_{};
+    // Multiple timers for different IDs by std::vector
+    std::vector<rclcpp::TimerBase::SharedPtr> timers_{};
+    std::vector<unsigned int> publishing_counters_{}; // Store frequencies for each timer
+
+    std::vector<std_msgs::msg::String> messages_{};
+  };
 }
 
 #endif
--- simple_node/src/simple_talker.cpp
+++ simple_node/src/simple_talker.cpp
@@ -1,6 +1,11 @@
 #include "simple_node/simple_talker.hpp"
 #include <rclcpp/rclcpp.hpp>
 #include <chrono>
+#include <std_msgs/msg/string.hpp>
+#include <random>
+#include <string>
+#include <vector>
+
 #include "yaml-cpp/yaml.h"
 
 namespace simple_node
@@ -17,7 +22,6 @@ SimpleTalker::SimpleTalker(const std::string &default_node_name, const YAML::Nod
   if (options.use_intra_process_comms()) {
     RCLCPP_INFO(this->get_logger(), "Intra-process communication is enabled for node '%s'.", node_name.c_str());
   }
-
 
   if (config["callbacks"] && config["callbacks"].IsSequence()) {
     init(config);
@@ -47,22 +51,28 @@ int SimpleTalker::init(const YAML::Node &config)
   {
     auto topic = cb_config["topic"].as<std::string>();
     auto frequency = cb_config["frequency"].as<double>();
-    auto message_text = cb_config["message"].as<std::string>();
     auto launch = true; // Default to not silent
     if (cb_config["launch"] && cb_config["launch"].IsScalar()) {
       launch = cb_config["launch"].as<bool>();
-    }    
+    }
+    bool append_count = cb_config["show_counter"].as<bool>(true);
+
+    // message will be reserved even if launch is false because the implementation is easier.
+    reserve_message(cb_config);
 
     if (launch) {
+      // in this case, this talker will publish messages with the given  topic.
       auto publisher = this->create_publisher<std_msgs::msg::String>(topic, 10);
       publishers_.push_back(publisher);
       publishing_counters_.push_back(static_cast<unsigned int>(0));
     
-      auto timer_callback = [this, publisher, message_text, callback_idx]() -> void {
-        auto message = std_msgs::msg::String();
-        message.data = message_text + " " + std::to_string(publishing_counters_[callback_idx]++);
+      // defining the timer callback.
+      auto timer_callback = [this, publisher,  callback_idx]() -> void {
+        auto & message = messages_[callback_idx];
+        publishing_counters_[callback_idx]++;
         publisher->publish(message);
       };
+
 
       auto timer = this->create_wall_timer(
         std::chrono::duration<double>(1.0 / frequency),
@@ -73,9 +83,11 @@ int SimpleTalker::init(const YAML::Node &config)
       RCLCPP_INFO(this->get_logger(), "Publishing to '%s' at %f Hz", topic.c_str(), frequency);
 
     } else {
+      // in this case, this talker will not publish messages with the given topic.
+      // only timer and counter will run.
       publishing_counters_.push_back(static_cast<unsigned int>(0));
 
-      auto timer_callback = [this, topic, message_text, callback_idx]() -> void
+      auto timer_callback = [this, callback_idx]() -> void
       {
         publishing_counters_[callback_idx]++;
       };
@@ -93,5 +105,92 @@ int SimpleTalker::init(const YAML::Node &config)
   }
 
   return 0;
+}
+
+void SimpleTalker::reserve_message(const YAML::Node &config)
+{
+
+  std_msgs::msg::String message_text;
+
+  MessagePattern message_pattern = static_cast<MessagePattern>(config["message_pattern"].as<unsigned int>(GIVEN_MESSAGE));
+
+  // if the given message pattern is not valid, default to GIVEN_MESSAGE
+  // if message is not set, use a default message
+  if (message_pattern < GIVEN_MESSAGE || message_pattern > RANDOM) {
+    message_pattern = GIVEN_MESSAGE; // Default to GIVEN_MESSAGE if invalid
+  }
+
+  // if message_pattern is GIVEN_MESSAGE, use the message from config straightforward.
+  if (message_pattern == GIVEN_MESSAGE) {
+    message_text.data = config["message"].as<std::string>("default message");
+    messages_.emplace_back(message_text);
+    return;
+  }
+
+  // if message_pattern is REPEAT or RANDOM, we need to reserve the message size.
+  size_t message_byte_size = config["message_bytes"].as<size_t>();
+  message_text.data.clear();
+  message_text.data.reserve(message_byte_size); // reserve is not recommended for std::string, but it is acceptable because it is just a sample code.
+
+  if (message_pattern == REPEAT) {
+    std::string given_message = config["message"].as<std::string>("default message");
+    repeat_message(given_message, message_byte_size, message_text.data);
+    messages_.emplace_back(message_text);
+    return;
+  }
+
+  if (message_pattern == RANDOM) {
+    std::string given_message = config["message"].as<std::string>("default message");
+    random_message(given_message, message_byte_size, message_text.data);
+    messages_.emplace_back(message_text);
+    return;
+  }
+}
+
+void SimpleTalker::repeat_message(const std::string &given_message, const size_t &message_byte_size, std::string & message_text) {
+
+
+  std::string repeated_message;
+  if (given_message.size() == 0) {
+    repeated_message = "default message";
+  } else {
+    repeated_message = given_message;
+  }
+
+  const size_t full_copies = message_byte_size / repeated_message.size();
+  const size_t remaining_bytes = message_byte_size % repeated_message.size();
+
+  for (size_t i = 0; i < full_copies; ++i) {
+    message_text += repeated_message;
+  }
+  if (remaining_bytes > 0) {
+    message_text += repeated_message.substr(0, remaining_bytes);
+  }
+
+  return;
+}
+
+void SimpleTalker::random_message(const std::string &given_message, const size_t &message_byte_size, std::string &message_text) {
+
+  message_text = given_message;
+
+  const std::string charset = "abcdefghijklmnopqrstuvwxyz"
+                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                              "0123456789";
+
+  std::random_device rd;
+  std::mt19937 generator(rd());
+  std::uniform_int_distribution<> distribution(0, charset.size() - 1);
+
+
+  // Randomly fill the message with characters until it reaches the desired byte size
+  while (message_text.size() < message_byte_size) {
+    message_text += charset[distribution(generator)];
+  }
+
+  if (message_text.size() > message_byte_size) {
+    message_text.resize(message_byte_size); // Trim to the exact byte size
+  }
+  return;
 }
 }
