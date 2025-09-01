da62fd028c29332dea6eed65ae7906d422e39023
Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
2025-09-01 10:47:56 +0900

    feat: config generation with topic num
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>

--- measurement_script/topic_size/create_config_yaml_for_specified_node_num_and_topic_size.py
+++ measurement_script/topic_size/create_config_yaml_for_specified_node_num_and_topic_size.py
@@ -22,50 +22,49 @@ import sys
 import yaml
 
 
-def generate_config_for_pub_sub(node_num: int, topic_size_byte_per_sec: int) -> dict:
-    config = {}
+def generate_config_for_pub_sub(topic_num: int, topic_size_byte_per_sec: int) -> dict:
     message_size_byte = topic_size_byte_per_sec // 10  # Assuming frequency is fixed at 10 Hz and 1 char = 1 byte = 8 bits
-    for i in range(node_num//2):
-        # talkerの設定
-        talker_name = f"talker_{i}"
-        config[talker_name] = {
-            "type": "talker",
-            "executor_type": "single_threaded",
-            "callbacks": [
-                {
-                    "topic": f"/topic/message/name{i}",
-                    "frequency": 10,
-                    "message": f"Hello from name{i}",
-                    "message_pattern": 1, # 0: As-is 1: Repeat, 2: Random
-                    "message_bytes": message_size_byte, # size of the message in bytes
-                    "show_count": False # 末尾に publish 回数を載せるか否か
-                }
-            ]
+    subscribers = []
+    publishers = []
+    for i in range(topic_num):
+        publisher = {
+            "topic": f"/topic/message/name{i}",
+            "frequency": 10,
+            "message": f"Hello from name{i}",
+            "message_pattern": 1, # 0: As-is 1: Repeat, 2: Random
+            "message_bytes": message_size_byte, # size of the message in bytes
+            "show_count": False # 末尾に publish 回数を載せるか否か
         }
-        # listenerの設定
-        listener_name = f"listener_{i}"
-        config[listener_name] = {
-            "type": "listener",
-            "executor_type": "single_threaded",
-            "silent": True,
-            "callbacks": [
-                {
-                    "topic": f"/topic/message/name{i}"
-                }
-            ]
+        publishers.append(publisher)
+        subscriber = {
+            "topic": f"/topic/message/name{i}"
         }
+        subscribers.append(subscriber)
+
+    config = {}
+    config["deluxe_talker"] = {
+        "type": "talker",
+        "executor_type": "single_threaded",
+        "callbacks": publishers
+    }
+    config["deluxe_listener"] = {
+        "type": "listener",
+        "executor_type": "single_threaded",
+        "silent": True,
+        "callbacks": subscribers
+    }
     return config
 
 
 def main():
     # 引数でノード数を取得
     if len(sys.argv) != 3:
-        print("Usage: python specified_node_num.py <node_num> <topic_size_byte_per_sec>")
+        print("Usage: python create_config_yaml_for_specified_node_num_and_topic_size.py <topic_num> <topic_size_byte_per_sec>")
         sys.exit(1)
     
-    node_num = int(sys.argv[1])
+    topic_num = int(sys.argv[1])
     topic_size_byte_per_sec = int(sys.argv[2])
-    config = generate_config_for_pub_sub(node_num, topic_size_byte_per_sec)
+    config = generate_config_for_pub_sub(topic_num, topic_size_byte_per_sec)
     # YAMLファイルに書き出し
     output_file = "config.yaml"
     with open(output_file, 'w') as file:
